ef6201d3cd5124ef45b2979518873f5045bbee97
jonabergamo <jonathanbergamo16@gmail.com>
2023-06-12 12:19:04 -0300

    update

--- src/pages/page02/index.jsx
+++ src/pages/page02/index.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from "react-router-dom";
 import { motion } from "framer-motion";
 import "./style.css";
 
@@ -73,6 +79,32 @@ export default function Page02() {
     pulse: false,
   });
 
+  const navGlith = useGlitch({
+    playMode: "hover",
+    createContainers: true,
+    hideOverflow: false,
+    timing: {
+      duration: 2000,
+    },
+    glitchTimeSpan: {
+      start: 0,
+      end: 1,
+    },
+    shake: {
+      velocity: 15,
+      amplitudeX: 0.2,
+      amplitudeY: 0.2,
+    },
+    slice: {
+      count: 6,
+      velocity: 15,
+      minHeight: 0.02,
+      maxHeight: 0.15,
+      hueRotate: true,
+    },
+    pulse: false,
+  });
+
   return (
     <motion.div
       className="content"
@@ -80,7 +112,33 @@ export default function Page02() {
       animate={{ opacity: 1 }}
     >
       <main>
-        <header><div className="logo"><h1 className="name">Jonathan Bergamo</h1></div></header>
+        <header>
+          <div className="logo">
+            <h1 className="name">Jonathan Bergamo</h1>
+          </div>
+          <div className="navbar">
+            <ol>
+              <a href="" ref={navGlith.ref}>
+                HOME
+              </a>
+            </ol>
+            <ol>
+              <a href="" ref={navGlith.ref}>
+                ABOUT
+              </a>
+            </ol>
+            <ol>
+              <a href="" ref={navGlith.ref}>
+                STUDIES
+              </a>
+            </ol>
+            <ol>
+              <a href="" ref={navGlith.ref}>
+                PROJECTS
+              </a>
+            </ol>
+          </div>
+        </header>
         <div className="first-look">
           <motion.img
             src={main}
@@ -98,7 +156,7 @@ export default function Page02() {
               color: "antiquewhite",
             }}
           >
-            SENAI
+            LIVE IN SP, BRAZIL
           </h2>
           <div className="background-text">
             <motion.h1
@@ -140,9 +198,26 @@ export default function Page02() {
           </div>
         </div>
         <div className="text-content">
+          <div className="about">
+            <h3 className="about-text">
+              Olá! Sou Jonathan Bergamo, um desenvolvedor criativo do Brasil.
+              Tenho paixão por criar soluções digitais inovadoras e impactantes,
+              proporcionando experiências excepcionais aos usuários. Meu
+              objetivo é combinar design visualmente atraente com usabilidade
+              eficiente, garantindo a criação de sites e aplicativos que se
+              destacam no mercado.
+            </h3>
+          </div>
           <RouterProvider router={router} />
         </div>
-        <footer><img src={signature} alt="" style={{ height: '15vw' }} ref={glitch.ref}/></footer>
+        <footer>
+          <img
+            src={signature}
+            alt=""
+            style={{ height: "15vw" }}
+            ref={glitch.ref}
+          />
+        </footer>
       </main>
     </motion.div>
   );
